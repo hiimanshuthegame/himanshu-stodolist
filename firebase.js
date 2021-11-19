c5ac4c1e7f8d0855135341f8d38334928ee61145
hiimanshuthegame <63640700+hiimanshuthegame@users.noreply.github.com>
2021-11-19 23:16:38 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAInjduCJe4B5CnOmjLRsDF7PNedwNvl6U",
+    authDomain: "himanshudemo-3230f.firebaseapp.com",
+    projectId: "himanshudemo-3230f",
+    storageBucket: "himanshudemo-3230f.appspot.com",
+    messagingSenderId: "145446122442",
+    appId: "1:145446122442:web:1198478cb607025ca81533",
+    measurementId: "G-NEL4F2J91L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
